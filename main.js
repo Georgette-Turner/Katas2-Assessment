011e387d0f1ee3b2de7cb597831479202a0550b7
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-05-29 00:41:44 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// Kata 01 Add
+
+function add(x, y) {
+    return x + y
+}
+// const result = sum(2, 4)
+console.log("Kata 1:"), add(2, 4)
+
+
+// Kata 02 Multiply use add funct
+function multiply(x, y) {
+    return
+}
+console.log()
+
+// Kata 03 Exponent
+function power(x, y) {git
+
+}
+console.log()
+
+
+// Kata 04 Factoral
+function Factoral() {
+
+}
+console.log
